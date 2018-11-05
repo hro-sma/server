6f1c7bbfde3f991e4cb7a88552aa93879656b46a
Niels van der Veer <n.i.vdveer@gmail.com>
2018-11-05 10:36:11 +0100

    Add commands to deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,12 +4,15 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy master
-
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh apps@$IP -p $PORT <<EOF
-  cd $DEPLOY_DIR
-  crystal build --release --no-debug index.cr # Change to whatever commands you need!
+ssh deploy@$IP -p $PORT <<EOF
+  cd ./weather.station.server
+  git fetch origin
+  git reset --hard origin/master
+  git pull
+  cd ./weather.station.server/weather.station.server
+  docker build --force-rm -t weatherserver .
+  docker stop weather-station-server || true
+  docker rm --force weather-station-server || true
+  docker run --restart=always --net=host --name=weather-station-server -d -t weatherserver
 EOF

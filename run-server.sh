8e873305ae528f108fef0b3e4584e69b71fa6e23
Niels van der Veer <n.i.vdveer@gmail.com>
2018-11-05 10:15:09 +0100

    Add .travis.yml for deployment

--- run-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git pull
-docker-compose -f weather.station.server/weather.station.server/docker-compose.yml down
-docker-compose -f weather.station.server/weather.station.server/docker-compose.yml build
-docker-compose -f weather.station.server/weather.station.server/docker-compose.yml up --force-recreate -d

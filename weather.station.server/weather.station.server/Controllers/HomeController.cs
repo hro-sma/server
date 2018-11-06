2af0251d1fc81af02261467984514369c1b45aaf
Sava Sharif <savaaa_@hotmail.com>
2018-11-06 11:05:16 +0100

    backend code to take latest updates

--- weather.station.server/weather.station.server/Controllers/HomeController.cs
+++ weather.station.server/weather.station.server/Controllers/HomeController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using weather.station.server.Data;
+using weather.station.server.Models;
 
 namespace weather.station.server.Controllers
 {
     public class HomeController : Controller
     {
-        private WeatherStationServerContext _context;
+        private readonly WeatherStationServerContext _context;
 
         public HomeController(WeatherStationServerContext context)
         {
@@ -19,6 +21,20 @@ namespace weather.station.server.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            if (_context.WeatherUpdate.Any())
+            {
+                var updates =
+                    _context.WeatherUpdate.Include(d => d.DeviceId).GroupBy(i => i.DeviceId); //get updates with the devices
+                var latestPerId = new Dictionary<Guid, WeatherUpdate>();
+                foreach (var group in updates)
+                {
+                    var key = group.Key;
+                    var test = group.OrderByDescending(d => d.TimeStamp).First();
+                    latestPerId.Add(key, test);
+                }
+            }
+          
+
             return View();
         }
     }

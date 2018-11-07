d02ed2a8090b6eedac2e85343340aba26a75b983
Niels Witte <wrlxs@hotmail.com>
2018-11-07 23:39:47 +0100

    Device dashboard - featuring graphs of temperature, humidty and windspeed.
    Accompanied by a search device page which allows you to easily access a specific dashboard.

---
+++ weather.station.server/weather.station.server/Models/ViewModels/DeviceSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weather.station.server.Models.ViewModels
+{
+    public class DeviceSearchViewModel
+    {
+        public Guid DeviceId { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
